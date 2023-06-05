6f4c95c17b0b2da74b4b99005a6a888d83b508b9
brookebrown617 <brownbrooke617@gmail.com>
2023-06-05 15:54:34 -0500

    added tests and created base class, extended subclasses

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo;
 
+import java.security.PublicKey;
 import java.util.Objects;
 
 public class Job {
@@ -16,9 +17,107 @@ public class Job {
     // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
+    public Job(){
+        id = nextId;
+        nextId++;
+    }
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency){
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+    public String toString(){
+        if(name.equals("")){
+            name = "Name not valid";
+        } else if(employer.getValue().equals("")) {
+            employer.setValue("Employer not valid");
+        } else if(location.getValue().equals("")) {
+            location.setValue("Location not valid");
+        } else if(positionType.getValue().equals("")) {
+            positionType.setValue("Position Type not valid");
+        } else if(coreCompetency.getValue().equals("")) {
+            coreCompetency.setValue("Core Competency not valid");
+        }
+        String output = String.format(
+                        "\nID: %d\n" +
+                        "\nName: %s\n" +
+                        "\nEmployer: %s\n" +
+                        "\nLocation: %s\n" +
+                        "\nPosition Type: %s\n" +
+                        "\nCore Competency: %s\n", id, name, employer, location, positionType, coreCompetency);
+        return output;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    public int getId() {
+        return id;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+
+
+
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
